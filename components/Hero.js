e1987c07c8399087012115e6c6eaa019920c2bc1
Медуша <Medusa@MacBook-Pro-4.local>
2023-03-16 18:10:33 +0800

    first commit

---
+++ components/Hero.js
@@ -0,0 +1,79 @@
+import Image from 'next/image';
+import Header from './Header';
+import { motion } from 'framer-motion';
+
+import {
+  staggerContainer,
+  girlAnim,
+  boyAnim,
+  heroTruckAnim,
+  fadeInDown,
+} from '../variants';
+
+import { CgArrowLongRight } from 'react-icons/cg';
+
+const Hero = ({ headerData, heroData, navData }) => {
+  const { title, boyImg, girlImg, truckImg, btnText } = heroData;
+  return (
+    <section className='bg-hero bg-no-repeat bg-left-top min-h-[800px] lg:min-h-[950px] lg:mb-80'>
+      {/* container */}
+      <motion.div
+        variants={staggerContainer}
+        initial='initial'
+        animate='animate'
+        className='container z-1 mx-auto relative min-h-[800px] lg:min-h-[950px]'
+      >
+        {/* header */}
+        <motion.div variants={fadeInDown}
+          className='fixed z-20 w-full'>
+          <Header headerData={headerData} navData={navData} />
+        </motion.div>
+
+        {/* title */}
+        <motion.h1
+          variants={fadeInDown}
+          className='h1 max-w-[740px] text-white pt-[12rem] mb-[60px]'
+        >
+          {title}
+        </motion.h1>
+
+        {/* button */}
+        <motion.button
+          whileHover={{
+            scale: 1.05,
+          }}
+          variants={fadeInDown}
+          className='btn'
+        >
+          {btnText} <CgArrowLongRight className='text-[30px]' />
+        </motion.button>
+
+        {/* girl image */}
+        <motion.div
+          variants={girlAnim}
+          className='hidden lg:flex absolute bottom-0'
+        >
+          <Image src={girlImg} width={206} height={495} />
+        </motion.div>
+
+        {/* truck image */}
+        <motion.div
+          variants={heroTruckAnim}
+          className='hidden lg:flex absolute -bottom-[25%] -left-[30%]'
+        >
+          <Image src={truckImg} width={811} height={395} />
+        </motion.div>
+
+        {/* boy image */}
+        <motion.div
+          variants={boyAnim}
+          className='hidden lg:flex absolute right-[380px] bottom-0 z-10'
+        >
+          <Image src={boyImg} width={206} height={495} />
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ components/Slider.js
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const Slider = ({ clients }) => {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={20}
+      slidesOffsetBefore={60}
+      grabCursor={true}
+      modules={[]}
+      className='testimonialSlider'
+    >
+      {clients.map((client, idx) => {
+        const { message, image, name } = client;
+        return (
+          <SwiperSlide key={idx}>
+            <div className='bg-white h-[360px] drop-shadow-primary rounded-[10px] px-[50px] pt-[60px] pb-[40px] flex flex-col justify-between'>
+              {/*  message */}
+              <p className='font-light leading-[30px]'>{message}</p>
+              {/* name & image */}
+              <div className='flex items-center gap-x-5'>
+                <Image src={image} width={60} height={60} />
+                <span className='font-semibold'>{name}</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
+export default Slider;
